538898b766f86baf87fc44bc7b73a3c2099328fb
Odisuck <jokokalbouneh@gmail.com>
2025-03-29 08:52:21 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+# Example file showing a basic pygame "game loop"
+import pygame
+
+
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((640, 360))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("#2a2d33")
+
+    # RENDER YOUR GAME HERE
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # Use deltatime, limits FPS to 144
+    dt = clock.tick(144) / 1000  
+
+pygame.quit()
